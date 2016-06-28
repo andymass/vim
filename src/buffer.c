020d7a3e2a87bf74b0734064eafe871988b096bf
misterfish <allen@netherrealm.net>
2016-06-28 21:57:26 +0200

    Run autocommand 'EVENT_DIRCHANGED' on autochdir.

--- src/buffer.c
+++ src/buffer.c
@@ -1771,8 +1771,17 @@ do_autochdir(void)
 {
     if ((starting == 0 || test_autochdir)
 	    && curbuf->b_ffname != NULL
-	    && vim_chdirfile(curbuf->b_ffname) == OK)
+	    && vim_chdirfile(curbuf->b_ffname) == OK) {
+	char_u dir[MAXPATHL];
+
 	shorten_fnames(TRUE);
+
+#ifdef FEAT_AUTOCMD
+	vim_strncpy(dir, curbuf->b_ffname, MAXPATHL - 1);
+	*gettail_sep(dir) = NUL;
+	apply_autocmds(EVENT_DIRCHANGED, dir, dir, FALSE, curbuf);
+#endif
+    }
 }
 #endif
 
