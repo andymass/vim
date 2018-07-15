9588ed9fe892efa46089d5eede64482419c0d1b2
Andy K. Massimino <massimino@gatech.edu>
2018-07-15 12:51:26 -0400

    Add split-move wincmds g[hjkl]

--- src/window.c
+++ src/window.c
@@ -19,6 +19,7 @@ static void frame_setwidth(frame_T *curfrp, int width);
 static void win_exchange(long);
 static void win_rotate(int, int);
 static void win_totop(int size, int flags);
+static void win_moveinto(int size, int vert, int above);
 static void win_equal_rec(win_T *next_curwin, int current, frame_T *topfr, int dir, int col, int row, int width, int height);
 static win_T *win_free_mem(win_T *win, int *dirp, tabpage_T *tp);
 static frame_T *win_altframe(win_T *win, tabpage_T *tp);
@@ -588,6 +589,17 @@ wingotofile:
 			nchar = xchar;
 			goto wingotofile;
 #endif
+
+		    /* move window into another window */
+		    case 'k':
+		    case 'j':
+		    case 'h':
+		    case 'l':
+			CHECK_CMDWIN;
+			win_moveinto((int)Prenum, xchar == 'h' || xchar == 'l',
+				xchar == 'h' || xchar == 'k');
+			break;
+
 		    default:
 			beep_flush();
 			break;
@@ -1689,6 +1701,91 @@ win_totop(int size, int flags)
 
     /* Split a window on the desired side and put the window there. */
     (void)win_split_ins(size, flags, curwin, dir);
+    if (!(flags & WSP_VERT))
+    {
+	win_setheight(height);
+	if (p_ea)
+	    win_equal(curwin, TRUE, 'v');
+    }
+
+#if defined(FEAT_GUI)
+    /* When 'guioptions' includes 'L' or 'R' may have to remove or add
+     * scrollbars.  Have to update them anyway. */
+    gui_may_update_scrollbars();
+#endif
+}
+
+/*
+ * Move the current window into a split of the window in a given direction
+ */
+    static void
+win_moveinto(int size, int vert, int above)
+{
+    int		dir;
+    int 	flags;
+    win_T	*targetwin;
+    win_T	*wp;
+    int		height = curwin->w_height;
+
+    if (ONE_WINDOW)
+    {
+	beep_flush();
+	return;
+    }
+
+    /* Target window is the neighbor in the wincmd h,j,k,l direction */
+    if (vert)
+	targetwin = win_hor_neighbor(curtab, curwin, above, 1);
+    else
+	targetwin = win_ver_neighbor(curtab, curwin, above, 1);
+
+    /* If no target window, behave like wincmd H,J,K,L */
+    if (targetwin == curwin)
+    {
+	win_totop(size, (vert ? WSP_VERT : 0)
+				| (above ? WSP_TOP : WSP_BOT));
+	return;
+    }
+
+    /* Make the split left/above depending on relative positions;
+     * - If the current window is at least as big as the target then
+     *   compare the cursor position and the midpoint of the target window
+     * - If the current window is smaller than the target
+     *   then compare the midpoints of the current and target windows */
+    if (vert)
+    {
+	if (curwin->w_winrow +
+		(curwin->w_height >= targetwin->w_height
+			? curwin->w_wrow : curwin->w_height / 2)
+		<= targetwin->w_winrow + targetwin->w_height / 2)
+	    flags = WSP_ABOVE;
+	else
+	    flags = WSP_BELOW;
+    }
+    else
+    {
+	if (curwin->w_wincol +
+		(curwin->w_width >= targetwin->w_width
+			? curwin->w_wcol : curwin->w_width / 2)
+		<= targetwin->w_wincol + targetwin->w_width / 2)
+	    flags = WSP_VERT | WSP_ABOVE;
+	else
+	    flags = WSP_VERT | WSP_BELOW;
+    }
+
+    /* Jump to the new window but remember the current one */
+    wp = curwin;
+    win_goto(targetwin);
+
+    /* Remove the old window and frame from the tree of frames. */
+    (void)winframe_remove(wp, &dir, NULL);
+    win_remove(wp, NULL);
+    last_status(FALSE);	    /* may need to remove last status line */
+    (void)win_comp_pos();   /* recompute window positions */
+
+    /* Split a window on the desired side and put the old window there. */
+    (void)win_split_ins(size, flags, wp, dir);
+
     if (!(flags & WSP_VERT))
     {
 	win_setheight(height);
@@ -4230,11 +4327,26 @@ win_goto_ver(
     int		up,		/* TRUE to go to win above */
     long	count)
 {
+    win_T	*wp = win_ver_neighbor(curtab, curwin, up, count);
+    if (wp != NULL)
+	win_goto(wp);
+}
+
+/*
+ * Get the above or below neighbor of the specified window.
+ */
+    win_T *
+win_ver_neighbor(
+    tabpage_T	*tp,
+    win_T	*wp,
+    int		up,		/* TRUE to find win above */
+    long	count)
+{
     frame_T	*fr;
     frame_T	*nfr;
     frame_T	*foundfr;
 
-    foundfr = curwin->w_frame;
+    foundfr = wp->w_frame;
     while (count--)
     {
 	/*
@@ -4244,7 +4356,7 @@ win_goto_ver(
 	fr = foundfr;
 	for (;;)
 	{
-	    if (fr == topframe)
+	    if (fr == tp->tp_topframe)
 		goto end;
 	    if (up)
 		nfr = fr->fr_prev;
@@ -4271,7 +4383,7 @@ win_goto_ver(
 		/* Find the frame at the cursor row. */
 		while (fr->fr_next != NULL
 			&& frame2win(fr)->w_wincol + fr->fr_width
-					 <= curwin->w_wincol + curwin->w_wcol)
+					 <= wp->w_wincol + wp->w_wcol)
 		    fr = fr->fr_next;
 	    }
 	    if (nfr->fr_layout == FR_COL && up)
@@ -4281,8 +4393,7 @@ win_goto_ver(
 	}
     }
 end:
-    if (foundfr != NULL)
-	win_goto(foundfr->fr_win);
+    return foundfr != NULL ? foundfr->fr_win : NULL;
 }
 
 /*
@@ -4293,11 +4404,26 @@ win_goto_hor(
     int		left,		/* TRUE to go to left win */
     long	count)
 {
+    win_T	*wp = win_hor_neighbor(curtab, curwin, left, count);
+    if (wp != NULL)
+	win_goto(wp);
+}
+
+/*
+ * Get the left or right neighbor of the specified window.
+ */
+    win_T *
+win_hor_neighbor(
+    tabpage_T	*tp,
+    win_T	*wp,
+    int		left,		/* TRUE to find left win */
+    long	count)
+{
     frame_T	*fr;
     frame_T	*nfr;
     frame_T	*foundfr;
 
-    foundfr = curwin->w_frame;
+    foundfr = wp->w_frame;
     while (count--)
     {
 	/*
@@ -4307,7 +4433,7 @@ win_goto_hor(
 	fr = foundfr;
 	for (;;)
 	{
-	    if (fr == topframe)
+	    if (fr == tp->tp_topframe)
 		goto end;
 	    if (left)
 		nfr = fr->fr_prev;
@@ -4334,7 +4460,7 @@ win_goto_hor(
 		/* Find the frame at the cursor row. */
 		while (fr->fr_next != NULL
 			&& frame2win(fr)->w_winrow + fr->fr_height
-					 <= curwin->w_winrow + curwin->w_wrow)
+					 <= wp->w_winrow + wp->w_wrow)
 		    fr = fr->fr_next;
 	    }
 	    if (nfr->fr_layout == FR_ROW && left)
@@ -4344,8 +4470,7 @@ win_goto_hor(
 	}
     }
 end:
-    if (foundfr != NULL)
-	win_goto(foundfr->fr_win);
+    return foundfr != NULL ? foundfr->fr_win : NULL;
 }
 
 /*
